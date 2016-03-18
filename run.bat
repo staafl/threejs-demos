e6d81d6324b95f7882eaa4a44f2d6f5712636c09
Velko Nikolov <velko.nikolov@gmail.com>
2016-03-19 01:48:15 +0200

    minor

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-start "" "http://trustingwolves.com/threejs/%1"
+@start "" "http://trustingwolves.com/threejs/%1"
 ::start "" "https://htmlpreview.github.io/?https://github.com/staafl/threejs-demos/blob/master/%1/index.html"
