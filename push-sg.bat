49c1db5adbe1e5392c8b166919bea6b138ad48e0
Velko Nikolov <velko.nikolov@gmail.com>
2016-04-29 20:22:28 +0100

    minor

--- push-sg.bat
+++ push-sg.bat
@@ -1,4 +1,5 @@
 @call git add --all
-@call git commit -m %*
+@if [%1] == [] @call git commit
+@if NOT [%1] == [] @call git commit -m %*
 @call git push ssh://trusting@ams7.siteground.eu:18765/home/trusting/public_html/threejs master
 @call git push origin master
